624af5f1762d2b34a87f43edad9266ed294e86a5
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-04-03 21:09:57 +0530

    Used useMemo hook to optimize performance

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { useState } from "react";
 import UseRefExample1 from "./UseRefExample1";
 import UseRefExample2 from "./UseRefExample2";
 import UseRefExample3 from "./UseRefExample3";
+import UseMemoExample from "./UseMemoExample";
 
 // import { UserContextProvider } from "./UserContextProvider";
 function App() {
   return (
-    <UseRefExample3 />
+    <UseMemoExample />
     // <UserContextProvider>
     //   <h1>Dashboard</h1>
     //   <Header />
