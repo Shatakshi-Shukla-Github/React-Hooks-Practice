8d0f7e2a24a66a3e509bf9a31cebe45f9aa38214
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-07-02 19:31:25 +0530

    Implemented useCallback Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
-import UseCallbackExample from './components/UseCallbackExample'
-import UseRefExample1 from "./components/UseRefExample1"
-import UseRefExample3 from './components/UseRefExample3'
+import ParentComponent from './components/ParentComponent'
+// import UseCallbackExample from './components/UseCallbackExample'
+// import UseRefExample1 from "./components/UseRefExample1"
+// import UseRefExample3 from './components/UseRefExample3'
 function App() {
 
 
   return (
     <div className="container mt-5">
-      <UseCallbackExample />
+      <ParentComponent />
     </div>
   )
 }
---
+++ src/components/ParentComponent.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState, useCallback } from "react"
+import Count from "./Count"
+import Button from "./Button"
+import Title from "./Title"
+export default function ParentComponent() {
+    const [age, setAge] = useState(25)
+    const [salary, setSalary] = useState(25000)
+    const incAge = useCallback(() => {
+        setAge(age + 1)
+    })
+    const incSalary = useCallback(() => {
+        setSalary(salary + 1000)
+    }, [])
+    return (
+        <div>
+            <Title />
+            <Count text="Age" count={age} />
+            <Button handleClick={incAge}>Increment Age</Button>
+            <Count text="Salary" count={salary} />
+            <Button handleClick={incSalary}>Increment Salary</Button>
+        </div>
+    )
+}
